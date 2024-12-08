4afe00b1409d60501e1139a830d3908b3f4c017a
Wesley <wesleyvc2110@gmail.com>
2024-12-07 22:59:00 -0300

    reafctor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+// function soma1() {
+//   const numbers = [1, 2, 3, 4];
+//   const soma = numbers.map((number) => {
+//     console.log(number);
+//   }).reduce((a, b) => a + b);
+//   console.log(soma);
+// } 
 const App = () => {
   return (
     <>
